506afe07b3cf50baf788c87925ae14a412d39faa
靳鸿媛 <jacelynfish@outlook.com>
2017-05-23 23:43:03 +0800

    modify container

--- src/components/container.vue
+++ src/components/container.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="container">
-        <hc></hc>
         <router-view></router-view>
     </div>
 </template>
@@ -8,39 +7,9 @@
 
 </style>
 <script>
-    const directives = [
-        { name: 'focus', expression:'dismiss', modifiers: { abc: true } }
-    ]
 
-
-
-    var hc = {
-        render (h){
-            return (
-                <div
-                    id="hello"
-                    class={{ hello: true, bye: false}}
-                    onClick={()=>alert("hi")}
-                     {...{directives}}
-                    >
-                hello from the other side
-                </div>
-            )
-        }
-    }
     export default{
-        data: function(){
-            return {
-                dismiss:'kkk',
-            }
-        },
-        components:{
-            hc,
-        },
-        mounted:function(){
-            console.log('hi');
+        data: function() {
         }
-
-
     }
 </script>
